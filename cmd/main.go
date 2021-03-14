cbff57866d60207eb092e6e0939db4df7721e340
Ignasi Fosch <natx@y10k.ws>
2021-03-14 11:13:55 +0100

    Add message processors
    Message processors are simple function receiving a `*Message` that
    will do something with it. Two processors are provided `LogMessage` and
    `printMessage`. The first one just logs it, and the second one prints
    the output.
    
    The `LogMessage` processor, defined in `slack` package, provides the
    same functionality as the original code was, i.e. logging the message
    event.
    
    The `printMessage` processor, defined in main program, prints the
    message event on stdout. This is provided just as an example.
    
    Both are called as corresponding goroutines.

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/ifosch/synthetic-david/pkg/slack"
 )
+
+func printMessage(msg *slack.Message) {
+	thread := ""
+	if msg.Thread {
+		thread = "a thread in "
+	}
+	fmt.Printf("Message: '%v' from '%v' in %v'%v'\n", msg.Text, msg.User.Name, thread, msg.Conversation.Name)
+}
 
 func main() {
 	slackToken, ok := os.LookupEnv("SLACK_TOKEN")
@@ -14,5 +23,6 @@ func main() {
 	}
 	debug := false
 	client := slack.NewChat(slackToken, debug)
+	client.RegisterMessageProcessor(printMessage)
 	client.Start()
 }
--- pkg/slack/slack.go
+++ pkg/slack/slack.go
@@ -7,9 +7,19 @@ import (
 	"github.com/slack-go/slack"
 )
 
+// LogMessage ...
+func LogMessage(msg *Message) {
+	thread := ""
+	if msg.Thread {
+		thread = "a thread in "
+	}
+	log.Printf("Message: '%v' from '%v' in %v'%v'\n", msg.Text, msg.User.Name, thread, msg.Conversation.Name)
+}
+
 // Chat is a ...
 type Chat struct {
-	api *slack.Client
+	api        *slack.Client
+	processors map[string][]func(*Message)
 }
 
 // NewChat ...
@@ -19,7 +29,20 @@ func NewChat(token string, debug bool) (chat *Chat) {
 		slack.OptionDebug(debug),
 		slack.OptionLog(log.New(os.Stdout, "slack-bot: ", log.Lshortfile|log.LstdFlags)),
 	)
-	return &Chat{api: api}
+	processors := map[string][]func(*Message){
+		"message": []func(*Message){},
+	}
+	chat = &Chat{
+		api:        api,
+		processors: processors,
+	}
+	chat.RegisterMessageProcessor(LogMessage)
+	return
+}
+
+// RegisterMessageProcessor ...
+func (c *Chat) RegisterMessageProcessor(processor func(*Message)) {
+	c.processors["message"] = append(c.processors["message"], processor)
 }
 
 // Start ...
@@ -28,19 +51,22 @@ func (c *Chat) Start() {
 	go rtm.ManageConnection()
 
 	for msg := range rtm.IncomingEvents {
-		switch ev := msg.Data.(type) {
-		case *slack.MessageEvent:
-			msg, err := ReadMessage(ev, c.api)
-			if err != nil {
-				log.Printf("Error %v processing message %v", err, ev)
-				break
-			}
-			if msg.Completed {
-				thread := ""
-				if msg.Thread {
-					thread = "a thread in "
-				}
-				log.Printf("Message: '%v' from '%v' in %v'%v'\n", msg.Text, msg.User.Name, thread, msg.Conversation.Name)
+		c.Process(msg)
+	}
+}
+
+// Process ...
+func (c *Chat) Process(msg slack.RTMEvent) {
+	switch ev := msg.Data.(type) {
+	case *slack.MessageEvent:
+		msg, err := ReadMessage(ev, c.api)
+		if err != nil {
+			log.Printf("Error %v processing message %v", err, ev)
+			break
+		}
+		if msg.Completed {
+			for _, processor := range c.processors["message"] {
+				go processor(msg)
 			}
 		}
 	}
