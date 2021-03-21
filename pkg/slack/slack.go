faf5a8dce78a36f1772e386e2ab4624486123c19
Ignasi Fosch <natx@y10k.ws>
2021-03-21 07:34:08 +0100

    Add events to manage in Process
    Some events are not relevant for now, for a few others, it seems wise
    to add some logging.

--- pkg/slack/slack.go
+++ pkg/slack/slack.go
@@ -75,5 +75,103 @@ func (c *Chat) Process(msg slack.RTMEvent) {
 				go processor(msg)
 			}
 		}
+	case *slack.ConnectingEvent:
+		log.Printf("Trying to connect to Slack: Attempt %v of %v", ev.Attempt, ev.ConnectionCount)
+	case *slack.ConnectedEvent:
+		log.Printf("Connected to %v Slack as %v after %v attempts ", ev.Info.Team.Name, ev.Info.User.Name, ev.ConnectionCount+1)
+	case *slack.InvalidAuthEvent:
+		log.Fatalf("Invalid credentials provided to Slack")
+	case *slack.ConnectionErrorEvent:
+		log.Printf("Error connecting to Slack %v", ev)
+	case *slack.DisconnectedEvent:
+		log.Printf("Disconnected event: %v", ev)
+	case *slack.IncomingEventError:
+		log.Printf("Unexpected error receiving a websocket event: %v", ev)
+	case *slack.MessageTooLongEvent:
+		log.Printf("Last message was too long: %v", ev)
+	case *slack.OutgoingErrorEvent:
+		log.Printf("Unspecific error on outgoing message: %v", ev)
+	case *slack.RTMError:
+		log.Printf("Unspecific error on RTM: %v", ev)
+	case *slack.RateLimitEvent:
+		log.Printf("Slack rate limit reached: %v", ev)
+	case *slack.UnmarshallingErrorEvent:
+		log.Printf("Unmarshalling error: %v", ev)
+	// Unmanaged events:
+	case *slack.AckMessage:
+	case *slack.BotAddedEvent:
+	case *slack.BotChangedEvent:
+	case *slack.ChannelArchiveEvent:
+	case *slack.ChannelCreatedEvent:
+	case *slack.ChannelDeletedEvent:
+	case *slack.ChannelHistoryChangedEvent:
+	case *slack.ChannelInfoEvent:
+	case *slack.ChannelJoinedEvent:
+	case *slack.ChannelLeftEvent:
+	case *slack.ChannelMarkedEvent:
+	case *slack.ChannelRenameEvent:
+	case *slack.ChannelUnarchiveEvent:
+	case *slack.CommandsChangedEvent:
+	case *slack.DNDUpdatedEvent:
+	case *slack.DesktopNotificationEvent:
+	case *slack.EmailDomainChangedEvent:
+	case *slack.EmojiChangedEvent:
+	case *slack.FileChangeEvent:
+	case *slack.FileCommentAddedEvent:
+	case *slack.FileCommentDeletedEvent:
+	case *slack.FileCommentEditedEvent:
+	case *slack.FileCreatedEvent:
+	case *slack.FileDeletedEvent:
+	case *slack.FilePrivateEvent:
+	case *slack.FilePublicEvent:
+	case *slack.FileSharedEvent:
+	case *slack.FileUnsharedEvent:
+	case *slack.GroupArchiveEvent:
+	case *slack.GroupCloseEvent:
+	case *slack.GroupCreatedEvent:
+	case *slack.GroupHistoryChangedEvent:
+	case *slack.GroupJoinedEvent:
+	case *slack.GroupLeftEvent:
+	case *slack.GroupMarkedEvent:
+	case *slack.GroupOpenEvent:
+	case *slack.GroupRenameEvent:
+	case *slack.GroupUnarchiveEvent:
+	case *slack.HelloEvent:
+	case *slack.IMCloseEvent:
+	case *slack.IMCreatedEvent:
+	case *slack.IMHistoryChangedEvent:
+	case *slack.IMMarkedEvent:
+	case *slack.IMMarkedHistoryChanged:
+	case *slack.IMOpenEvent:
+	case *slack.LatencyReport:
+	case *slack.ManualPresenceChangeEvent:
+	case *slack.MemberJoinedChannelEvent:
+	case *slack.MemberLeftChannelEvent:
+	case *slack.MobileInAppNotificationEvent:
+	case *slack.PinAddedEvent:
+	case *slack.PinRemovedEvent:
+	case *slack.Ping:
+	case *slack.Pong:
+	case *slack.PrefChangeEvent:
+	case *slack.PresenceChangeEvent:
+	case *slack.ReactionAddedEvent:
+	case *slack.ReactionRemovedEvent:
+	case *slack.ReconnectUrlEvent:
+	case *slack.StarAddedEvent:
+	case *slack.StarRemovedEvent:
+	case *slack.SubteamCreatedEvent:
+	case *slack.SubteamMembersChangedEvent:
+	case *slack.SubteamSelfAddedEvent:
+	case *slack.SubteamSelfRemovedEvent:
+	case *slack.SubteamUpdatedEvent:
+	case *slack.TeamDomainChangeEvent:
+	case *slack.TeamJoinEvent:
+	case *slack.TeamMigrationStartedEvent:
+	case *slack.TeamPrefChangeEvent:
+	case *slack.TeamRenameEvent:
+	case *slack.UserChangeEvent:
+	case *slack.UserTypingEvent:
+	default:
+		log.Printf("Unexpected event (%T): %v", ev, ev)
 	}
 }
