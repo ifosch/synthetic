74f67a7c73163e0484fe5c3cc32f02be3de54fbf
Ignasi Fosch <natx@y10k.ws>
2021-03-27 11:14:40 +0100

    Add helper to remove a word from a text

--- pkg/slack/helpers.go
+++ pkg/slack/helpers.go
@@ -3,9 +3,26 @@ package slack
 import (
 	"reflect"
 	"runtime"
+	"strings"
 )
 
 // getProcessorName ...
 func getProcessorName(f interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
+}
+
+// RemoveWord ...
+func RemoveWord(text string, word string) string {
+	slice := strings.Split(text, " ")
+	i := -1
+	for k, v := range slice {
+		if v == word {
+			i = k
+			break
+		}
+	}
+	if i >= 0 {
+		slice = append(slice[:i], slice[i+1:]...)
+	}
+	return strings.Join(slice, " ")
 }
