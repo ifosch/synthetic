1ba03cf233c01bb888404fabd6dd566e5781eb1d
Ignasi Fosch <natx@y10k.ws>
2021-10-18 21:13:09 +0200

    Add `list clusters` command
    This new command provides a list of all the Kubernetes clusters known
    by the bot, i.e. defined in the bot's `$KUBECONFIG`.
    
    The kubeconfig load procedure is mid way to a better structure to
    provide cluster selection for newer commands, but requires some more working.

---
+++ pkg/k8s/k8s_test.go
@@ -0,0 +1,80 @@
+package k8s
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func stringIn(item string, items []string) bool {
+	for _, i := range items {
+		if item == i {
+			return true
+		}
+	}
+	return false
+}
+
+func compareStringLists(a, b []string) error {
+	for _, item := range a {
+		if !stringIn(item, b) {
+			return fmt.Errorf("item %s was not expected", item)
+		}
+	}
+	for _, item := range b {
+		if !stringIn(item, a) {
+			return fmt.Errorf("item %s is missing", item)
+		}
+	}
+
+	return nil
+}
+
+func TestListClusters(t *testing.T) {
+	curDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	tcs := []struct {
+		kubeconfig     string
+		expectedErrMsg string
+	}{
+		{
+			kubeconfig:     filepath.Join(curDir, "../../tests/fixtures/kubeconfig.yaml"),
+			expectedErrMsg: "",
+		},
+		{
+			kubeconfig:     filepath.Join(curDir, "../../tests/fixtures/kubeconfig_one_missing.yaml"),
+			expectedErrMsg: "item cluster3.example.com is missing",
+		},
+		{
+			kubeconfig:     filepath.Join(curDir, "../../tests/fixtures/kubeconfig_one_unexpected.yaml"),
+			expectedErrMsg: "item unexpected.example.com was not expected",
+		},
+	}
+
+	for _, test := range tcs {
+		os.Setenv("KUBECONFIG", test.kubeconfig)
+		expectedClusters := []string{
+			"cluster1.example.com",
+			"cluster2.example.com",
+			"cluster3.example.com",
+		}
+
+		clusters, err := GetClusters()
+		if err != nil {
+			if err.Error() != test.expectedErrMsg {
+				t.Fatalf("%s", err)
+			}
+		}
+
+		err = compareStringLists(clusters, expectedClusters)
+		if err != nil {
+			if err.Error() != test.expectedErrMsg {
+				t.Fatalf("%s", err)
+			}
+		}
+	}
+}
