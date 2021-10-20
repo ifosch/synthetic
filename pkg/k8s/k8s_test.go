976081dc90e2cfda9578761d2df7f27d425dfc7c
Ignasi Fosch <natx@y10k.ws>
2021-10-20 07:12:45 +0200

    Add context selector to getConfig
    The function `getConfig` now requires one string parameter to select
    the K8s' context to connect to. With an empty string, it will use the
    default current context defined in kubecfg.
    
    There is no way in `clientcmd.ClientConfig` to select a different
    context to use once it was loaded. Even the client interfaces
    provixded by the `client-go` package are missing such
    feature.
    
    Building a whole configuration object to support things like this,
    looked like a harder path to take from this point. Particularly, as
    the code is to be run on every command anyway.

--- pkg/k8s/k8s_test.go
+++ pkg/k8s/k8s_test.go
@@ -40,6 +40,40 @@ func getKubeCfgFixture(filename string) string {
 	return filepath.Join(curDir, fmt.Sprintf("../../tests/fixtures/%s", filename))
 }
 
+func TestGetConfig(t *testing.T) {
+	tcs := []struct {
+		kubeconfig    string
+		chosenContext string
+		expectedHost  string
+	}{
+		{
+			kubeconfig:    getKubeCfgFixture("kubeconfig.yaml"),
+			chosenContext: "",
+			expectedHost:  "https://cluster1.example.com",
+		},
+		{
+			kubeconfig:    getKubeCfgFixture("kubeconfig.yaml"),
+			chosenContext: "cluster3.example.com",
+			expectedHost:  "https://cluster3.example.com",
+		},
+	}
+
+	for _, test := range tcs {
+		os.Setenv("KUBECONFIG", test.kubeconfig)
+
+		kubeConfig := getConfig(test.chosenContext)
+
+		clientConfig, err := kubeConfig.ClientConfig()
+		if err != nil {
+			panic(err)
+		}
+
+		if clientConfig.Host != test.expectedHost {
+			t.Fatalf("Incorrect host: got %s, expected %s", clientConfig.Host, test.expectedHost)
+		}
+	}
+}
+
 func TestListClusters(t *testing.T) {
 	tcs := []struct {
 		kubeconfig     string
