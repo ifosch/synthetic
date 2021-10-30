823042c4abd05ad60723e4ec64e5f01d6f4a833c
Ignasi Fosch <natx@y10k.ws>
2021-10-30 12:07:17 +0200

    Improve jenkinsJobTC
    By moving the `gojenkins.Job` creation into the test case, the test
    code is clearer and easier to understand.
    
    Adding a helper function to calculate expected Describe results also
    makes the tests easier to understand and to write.

--- pkg/job_control/jenkins_job_test.go
+++ pkg/job_control/jenkins_job_test.go
@@ -1,84 +1,128 @@
 package jobcontrol
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/bndr/gojenkins"
 )
 
 type jenkinsJobTC struct {
-	input            string
-	job              *gojenkins.Job
-	expectedDescribe string
+	input       string
+	name        string
+	description string
+	params      []struct {
+		Name         string
+		Type         string
+		Description  string
+		DefaultValue string
+	}
+}
+
+func (tc jenkinsJobTC) Describe() string {
+	expectedDescription := fmt.Sprintf(
+		"`%s`: %s\nParameters:\n",
+		tc.name,
+		tc.description,
+	)
+
+	for _, param := range tc.params {
+		expectedDescription += fmt.Sprintf(
+			"- *%s* (%s): %s (Default: `%s`)\n",
+			param.Name,
+			param.Type,
+			param.Description,
+			param.DefaultValue,
+		)
+	}
+
+	return expectedDescription
+}
+
+func (tc jenkinsJobTC) getJenkinsJob() *gojenkins.Job {
+	parameterDefinitions := []gojenkins.ParameterDefinition{}
+	for _, param := range tc.params {
+		parameterDefinition := gojenkins.ParameterDefinition{
+			Description: param.Description,
+			Name:        param.Name,
+			Type:        param.Type,
+			DefaultParameterValue: struct {
+				Name  string      `json:"name"`
+				Value interface{} `json:"value"`
+			}{
+				Name:  "",
+				Value: param.DefaultValue,
+			},
+		}
+		parameterDefinitions = append(parameterDefinitions, parameterDefinition)
+	}
+	return &gojenkins.Job{
+		Raw: &gojenkins.JobResponse{
+			Name:        tc.name,
+			Description: tc.description,
+			Property: []struct {
+				ParameterDefinitions []gojenkins.ParameterDefinition `json:"parameterDefinitions"`
+			}{
+				{
+					ParameterDefinitions: parameterDefinitions,
+				},
+			},
+		},
+	}
 }
 
 func TestJob(t *testing.T) {
 	tcs := []jenkinsJobTC{
 		{
-			input: "Simple job without params",
-			job: &gojenkins.Job{
-				Raw: &gojenkins.JobResponse{
-					Name:        "myjob",
-					Description: "myjob does something",
-				},
-			},
-			expectedDescribe: "`myjob`" + `: myjob does something
-Parameters:
-`,
+			input:       "Simple job without params",
+			name:        "myjob",
+			description: "myjob does something",
+			params: []struct {
+				Name         string
+				Type         string
+				Description  string
+				DefaultValue string
+			}{},
 		},
 		{
-			input: "Simple job with one param",
-			job: &gojenkins.Job{
-				Raw: &gojenkins.JobResponse{
-					Name:        "myjob",
-					Description: "myjob does something",
-					Property: []struct {
-						ParameterDefinitions []gojenkins.ParameterDefinition `json:"parameterDefinitions"`
-					}{
-						{
-							ParameterDefinitions: []gojenkins.ParameterDefinition{
-								{
-									Description: "myParam helps parametrize myjob",
-									Name:        "myParam",
-									Type:        "StringParameterDefinition",
-									DefaultParameterValue: struct {
-										Name  string      `json:"name"`
-										Value interface{} `json:"value"`
-									}{
-										"myParam",
-										"all",
-									},
-								},
-							},
-						},
-					},
+			input:       "Simple job with one param",
+			name:        "myjob",
+			description: "myjob does something",
+			params: []struct {
+				Name         string
+				Type         string
+				Description  string
+				DefaultValue string
+			}{
+				{
+					Name:         "myParam",
+					Type:         "StringParameterDefinition",
+					Description:  "myParam helps parametrize myjob",
+					DefaultValue: "all",
 				},
 			},
-			expectedDescribe: "`myjob`" + `: myjob does something
-Parameters:
-- *myParam* (StringParameterDefinition): myParam helps parametrize myjob (Default: ` + "`all`)\n",
 		},
 	}
 
 	for _, tc := range tcs {
 		t.Run(tc.input, func(t *testing.T) {
 			j := &Job{
-				jenkinsJob: tc.job,
+				jenkinsJob: tc.getJenkinsJob(),
 			}
 
 			name := j.Name()
-			if name != tc.job.Raw.Name {
-				t.Errorf("Wrong job name '%v' should be '%v'", name, tc.job.Raw.Name)
+			if name != tc.name {
+				t.Errorf("Wrong job name '%v' should be '%v'", name, tc.name)
 			}
 
 			description := j.Description()
-			if description != tc.job.Raw.Description {
-				t.Errorf("Wrong job description '%v' should be '%v'", description, tc.job.Raw.Description)
+			if description != tc.description {
+				t.Errorf("Wrong job description '%v' should be '%v'", description, tc.description)
 			}
 
 			describe := j.Describe()
-			if describe != tc.expectedDescribe {
-				t.Errorf("Wrong job describe '%v' should be '%v'", describe, tc.expectedDescribe)
+			if describe != tc.Describe() {
+				t.Errorf("Wrong job describe '%v' should be '%v'", describe, tc.Describe())
 			}
 		})
 	}
