dfce84644dffb641b17381a0cf70f35fab2f7aa3
Ignasi Fosch <natx@y10k.ws>
2021-10-30 09:08:55 +0200

    Add one more test case for Job object
    The new test case validates the case in which the job definition
    includes one parameter.
    
    I used this to adjust the test in a better and more clear way.

--- pkg/job_control/jenkins_job_test.go
+++ pkg/job_control/jenkins_job_test.go
@@ -7,37 +7,79 @@ import (
 )
 
 type jenkinsJobTC struct {
+	input            string
 	job              *gojenkins.Job
 	expectedDescribe string
 }
 
 func TestJob(t *testing.T) {
-	tc := jenkinsJobTC{
-		job: &gojenkins.Job{
-			Raw: &gojenkins.JobResponse{
-				Name:        "myjob",
-				Description: "myjob does something",
+	tcs := []jenkinsJobTC{
+		{
+			input: "Simple job without params",
+			job: &gojenkins.Job{
+				Raw: &gojenkins.JobResponse{
+					Name:        "myjob",
+					Description: "myjob does something",
+				},
 			},
+			expectedDescribe: "`myjob`" + `: myjob does something
+Parameters:
+`,
 		},
-		expectedDescribe: "`myjob`: myjob does something\nParameters:\n",
-	}
-	j := &Job{
-		jenkinsJob: tc.job,
+		{
+			input: "Simple job with one param",
+			job: &gojenkins.Job{
+				Raw: &gojenkins.JobResponse{
+					Name:        "myjob",
+					Description: "myjob does something",
+					Property: []struct {
+						ParameterDefinitions []gojenkins.ParameterDefinition `json:"parameterDefinitions"`
+					}{
+						{
+							ParameterDefinitions: []gojenkins.ParameterDefinition{
+								{
+									Description: "myParam helps parametrize myjob",
+									Name:        "myParam",
+									Type:        "StringParameterDefinition",
+									DefaultParameterValue: struct {
+										Name  string      `json:"name"`
+										Value interface{} `json:"value"`
+									}{
+										"myParam",
+										"all",
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			expectedDescribe: "`myjob`" + `: myjob does something
+Parameters:
+- *myParam* (StringParameterDefinition): myParam helps parametrize myjob (Default: ` + "`all`)\n",
+		},
 	}
 
-	name := j.Name()
+	for _, tc := range tcs {
+		t.Run(tc.input, func(t *testing.T) {
+			j := &Job{
+				jenkinsJob: tc.job,
+			}
 
-	description := j.Description()
+			name := j.Name()
+			if name != tc.job.Raw.Name {
+				t.Errorf("Wrong job name '%v' should be '%v'", name, tc.job.Raw.Name)
+			}
 
-	describe := j.Describe()
+			description := j.Description()
+			if description != tc.job.Raw.Description {
+				t.Errorf("Wrong job description '%v' should be '%v'", description, tc.job.Raw.Description)
+			}
 
-	if name != tc.job.Raw.Name {
-		t.Errorf("Wrong job name '%v' should be '%v'", name, tc.job.Raw.Name)
-	}
-	if description != tc.job.Raw.Description {
-		t.Errorf("Wrong job description '%v' should be '%v'", description, tc.job.Raw.Description)
-	}
-	if describe != tc.expectedDescribe {
-		t.Errorf("Wrong job describe '%v' should be '%v'", describe, tc.expectedDescribe)
+			describe := j.Describe()
+			if describe != tc.expectedDescribe {
+				t.Errorf("Wrong job describe '%v' should be '%v'", describe, tc.expectedDescribe)
+			}
+		})
 	}
 }
